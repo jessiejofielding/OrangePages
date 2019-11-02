c9177ea85805b5046dbbd712e52d2807f16b2178
zexin <zkoh@princeton.edu>
2019-11-01 23:11:52 -0400

    altered arguments in views.py

--- orangepages/views.py
+++ orangepages/views.py
@@ -7,28 +7,30 @@ from flask_login import current_user, login_required
 def feed():
     # # TODO:
 
-@app.route('/feed/post', methods=['GET'])
-def feed_post():
+@app.route('/search', methods=['GET'])
+def search():
+    # # TODO:
+    return render_template('search_preview.html',
+    user_preview_list=user_preview_list)
+
+@app.route('/post/<int:post_id>', methods=['GET'])
+def feed_post(post_id):
     # # TODO:
 
 @app.route('/create-post', methods=['POST'])
 def create_post():
     # # TODO:
 
-@app.route('/post=_/comment', methods=['POST'])
-def comment():
+@app.route('/post/<int:post_id>/comment', methods=['POST'])
+def comment(post_id):
     # # TODO:
 
-@app.route('/post=_/like', methods=['POST'])
-def like():
+@app.route('/post/<int:post_id>/like', methods=['POST'])
+def like(post_id):
     # # TODO:
 
-@app.route('/search', methods=['GET'])
-def search():
-    # # TODO:
-
-@app.route('/view-profile', methods=['GET'])
-def view_profile():
+@app.route('/user/<string:lookup_id>', methods=['GET'])
+def view_profile(lookup_id):
     # # TODO:
 
 @app.route('/create-user', methods=['POST'])
@@ -37,8 +39,4 @@ def create_user():
 
 @app.route('/edit-user', methods=['PUT'])
 def edit_user():
-    # # TODO:
-
-@app.route('/create-user/')
-def create_user():
     # # TODO:

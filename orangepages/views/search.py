f5152e046eb7bca41ae1e071404579929fcd697c
Jamie Guo <jamieguo@princeton.edu>
2019-11-14 20:00:11 -0500

    oopsi

--- orangepages/views/search.py
+++ orangepages/views/search.py
@@ -1,6 +1,8 @@
 from flask import request, make_response
 from flask import Blueprint, render_template
 from orangepages.models.models import User
+from orangepages.views.util import render
+
 
 page = Blueprint('testsearch', __name__)
 
@@ -12,6 +14,6 @@ def search_user():
     user_preview_list = User.search(*query_list).all()
     query = ' '.join(query_list)
 
-    return render_template('search.html',
+    return render('search.html',
     user_preview_list=user_preview_list, query=query, count=len(user_preview_list))
 

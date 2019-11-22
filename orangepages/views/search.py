5d53c6338d0903ddca4e0069c2318f13943e51d8
zexin <zkoh@princeton.edu>
2019-11-21 21:17:20 -0500

    Multipurpose search

--- orangepages/views/search.py
+++ orangepages/views/search.py
@@ -13,31 +13,29 @@ def search_user():
     query_list = request.args.get('query').split(' ')
 
     # FIXME sry - zx
-    if len(query_list) > 0 and len(query_list[0]) > 0 and query_list[0][0]=='#':
-        tagged_posts = []
 
-        for query in query_list:
-            if query[0] == '#':
-                print("hash", query[1:])
-                posts = search_tag(query[1:])
-                tagged_posts.extend(posts)
-
-        return render('test/all_posts.html', posts=tagged_posts)
-
+    posts = search_tag(query_list)
     user_preview_list = User.search(*query_list).all()
     query = ' '.join(query_list)
 
-    return render('search.html',
+    return render('test/all_posts.html', posts=posts,
     user_preview_list=user_preview_list, query=query, count=len(user_preview_list))
 
 # helper method
-def search_tag(tag_str):
-    tag = Tag.query.get(tag_str)
-    if tag == None:
-        posts = []
-    else:
-        posts = tag.get_posts()
-    return posts
+def search_tag(query_list):
+    tagged_posts = []
+    for query in query_list:
+        print("hash", query)
+
+        tag = Tag.query.get(query)
+        if tag == None:
+            posts = []
+        else:
+            posts = tag.get_posts()
+
+        tagged_posts.extend(posts)
+
+    return tagged_posts
 
 # @page.route('/searchbar-tag')
 # def searchbar_tag():

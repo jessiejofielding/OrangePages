d0496cd3c47c0abd2f33cdf591ffd4e7c981e8bc
Elise Colter <elisecolter@gmail.com>
2019-11-21 21:54:01 -0500

    counts

--- orangepages/views/search.py
+++ orangepages/views/search.py
@@ -19,7 +19,7 @@ def search_user():
     query = ' '.join(query_list)
 
     return render('search.html', posts=posts,
-    user_preview_list=user_preview_list, query=query, count=len(user_preview_list))
+    user_preview_list=user_preview_list, query=query)
 
 # helper method
 def search_tag(query_list):

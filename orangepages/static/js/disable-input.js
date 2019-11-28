0828dad30cfa1e558022e554e90ed301f713eda5
Julie Kallini <jkallini@princeton.edu>
2019-11-28 14:50:44 -0500

    Fix enter keypress in tags input

---
+++ orangepages/static/js/disable-input.js
@@ -0,0 +1,7 @@
+function stopRKey(evt) {
+    var evt = (evt) ? evt : ((event) ? event : null);
+    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+    if ((evt.keyCode == 13) && (node.type == "text")) { return false; }
+}
+
+document.onkeypress = stopRKey;

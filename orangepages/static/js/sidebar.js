1a6d85a497a6426eccaf75343a670b3f7e51b3e2
Julie Kallini <jkallini@princeton.edu>
2019-12-23 19:00:31 -0800

    Add sidebar to user Profile page

---
+++ orangepages/static/js/sidebar.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $('#dismiss').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});

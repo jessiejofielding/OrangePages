2b2da10e4ae8df45d99ca6c5e208d6be82eb7662
Jamie Guo <jamieguo@princeton.edu>
2020-01-03 13:18:11 -0500

    error pages redirect to home if not signed in bc message.html now requires user

--- orangepages/views/error.py
+++ orangepages/views/error.py
@@ -5,7 +5,7 @@ from flask_cas_fix import login, logout, login_required
 
 from orangepages.models.models import User
 from orangepages import cas
-from orangepages.views.util import render
+from orangepages.views.util import render, user_required
 
 
 
@@ -14,18 +14,21 @@ page = Blueprint('error', __name__)
 
 
 @page.app_errorhandler(404)
+@user_required
 def not_found(e):
     return render('message.html',
             title='Oops.',
             message="This page doesn't exist."), 404
 
 @page.app_errorhandler(500)
+@user_required
 def server_error(e):
     return render('message.html',
             title='A server error occurred.',
             message="Please try again later?"), 500
 
 @page.app_errorhandler(405)
+@user_required
 def not_allowed(e):
     return render('message.html',
             title='Not allowed',

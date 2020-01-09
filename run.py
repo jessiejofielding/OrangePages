19c2d7ac2006cac593cd6cb02c87b1e04e7451b5
Jessie <jessiejofielding@gmail.com>
2020-01-09 17:58:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,26 @@ from orangepages import app, admin
 from flask_admin.contrib.sqla import ModelView
 from orangepages.models.models import db, User, Post, Relationship, Group, Comment, Tag, Notification
 
+import orangepages.views.util as util
+from flask_admin.contrib import sqla
+
+class ModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        if util.cur_user() is None:
+            return False
+
+        if util.cur_uid() in ['jjf4', 'jamieguo', 'zkoh', 'ccolter', 'jkallini']:
+            return True
+
+        return False
+        
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect("orangepages.herokuapp.com")
+
+
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Group, db.session))

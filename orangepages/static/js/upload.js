1317098a36a7d061b2ec023ba0d2854f8992ba30
Jamie Guo <jamieguo@princeton.edu>
2020-01-10 13:33:36 -0500

    remove upload console log msg

--- orangepages/static/js/upload.js
+++ orangepages/static/js/upload.js
@@ -35,9 +35,6 @@ $(function () {
         }
     });
 
-    $('#imageResult').on('error', function () { 
-        console.log('img err');
-    })
 });
 
 /*  ==========================================
